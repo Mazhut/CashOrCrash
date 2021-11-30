f5d14904d380a38989defebbaa69ac3246cd70f5
Mazhut <alxqwerty@gmail.com>
2021-11-30 13:51:49 +0300

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("ubuntu-bold.ttf")]
+[assembly: ExportFont("sulphur-bold.ttf")]
+[assembly: ExportFont("sulphur-regular.ttf")]
+[assembly: ExportFont("teko-regular.ttf")]
+[assembly: ExportFont("teko-semibold.ttf")]
+
+namespace Testery
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
